867ad4b9f63ad1a36d3335de48f780f4712c9a78
Khushboo Kumari <khushboo.kumari@nusummit.com>
2026-05-24 22:37:12 +0530

    Pages creation done

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        # checking existing user
+        if User.objects.filter(username=username).exists():
+            return render(request, 'register.html', {'error': 'Username already exists'})
+
+        # creating user
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        return redirect('login')
+    return render(request, 'register.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Authenticate User
+        user = authenticate(
+            request, username = username, password = password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+
+        else:
+            return render(request, 'login.html', {
+                'error': 'Invalid credentials'
+            })
+        return render(request, 'login.html')    
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')    
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
